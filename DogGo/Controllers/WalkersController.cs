644563c7df3731c2752174f8ed8497a76714f7f5
Jessica Sturgell <sturgell01@gmail.com>
2024-08-20 11:04:30 -0400

    added model for owner

--- DogGo/Controllers/WalkersController.cs
+++ DogGo/Controllers/WalkersController.cs
@@ -23,9 +23,17 @@ namespace DogGo.Controllers
         }
 
         // GET: WalkersController/Details/5
+        // GET: Walkers/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Walker walker = _walkerRepo.GetWalkerById(id);
+
+            if (walker == null)
+            {
+                return NotFound();
+            }
+
+            return View(walker);
         }
 
         // GET: WalkersController/Create
